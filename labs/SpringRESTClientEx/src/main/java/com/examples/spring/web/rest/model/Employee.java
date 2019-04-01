089aa41168d316dbe61e43597c5e3f90137979ae
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2019-04-01 10:54:10 +0530

    Spring REST EmployMgmtApp Client example

--- labs/SpringRESTClientEx/src/main/java/com/examples/spring/web/rest/model/Employee.java
+++ labs/SpringRESTClientEx/src/main/java/com/examples/spring/web/rest/model/Employee.java
@@ -2,6 +2,9 @@ package com.examples.spring.web.rest.model;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Employee {
 
 	private String id;
